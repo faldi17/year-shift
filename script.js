b40b0696b09ddf513bab8e484be1bd0f4617cb40
Faldiansyah <faldiansyahk@gmail.com>
2025-01-02 13:12:31 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let h2 = document.querySelector('h2');
+let container = document.querySelector('.container');
+
+h2.onclick = function() {
+    container.classList.toggle('newyear');
+    this.classList.toggle('active');
+}
